a901d9209b46faec4eddc81a3c0d8558e3c7bad3
Tony Lee <hy.tony.lee@gmail.com>
2020-03-17 13:49:05 -0700

    Define routes

--- server.js
+++ server.js
@@ -14,5 +14,10 @@ app.get('/', (req, res) => {
 	res.json({ msg: 'Welcome to Daytrack WebApp API!' });
 });
 
+// Define Routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/task', require('./routes/tasks'));
+
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Servert started at the port ${PORT}`));
