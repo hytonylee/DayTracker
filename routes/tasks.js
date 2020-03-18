ab688eb976778b7711594c6d3586b7b1270b81da
Tony Lee <hy.tony.lee@gmail.com>
2020-03-18 13:10:59 -0700

    Add middleware auth

--- routes/tasks.js
+++ routes/tasks.js
@@ -3,8 +3,17 @@ const router = express.Router();
 const { check, validationResult } = require('express-validator');
 const Task = require('../models/Task');
 
+// Get all tasks
+
+// Get a User's tasks
 router.get('/', (req, res) => {
 	res.send('Welcome to the browser!');
 });
+
+// Get a User's Single task
+
+// Update a User's task
+
+// Delete a User's task
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -3,15 +3,75 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
+const auth = require('../middleware/auth');
+const { check, validationResult } = require('express-validator');
 
-// Login
-router.get('/login', (req, res) => {
-	res.send('Login');
-});
+const User = require('../models/User');
 
-// Register
-router.get('/register', (req, res) => {
-	res.send('Register');
-});
+// @route  POST api/users
+// @desc	 Register a user
+// @access Public
+router.post(
+	'/',
+	[
+		check('name', 'Please add a name!')
+			.not()
+			.isEmpty(),
+		check('email', 'Please include valid email address!').isEmail(),
+		check(
+			'password',
+			'Please enter a password with 6 or more characters'
+		).isLength({ min: 6 })
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		const { name, email, password } = req.body;
+
+		try {
+			let user = await User.findOne({ email });
+
+			if (user) {
+				return res.status(400).json({ msg: 'User already exists!' });
+			}
+
+			use = new User({
+				name,
+				email,
+				password
+			});
+
+			const salt = await bcrypt.genSalt(10);
+			user.password = await bcrypt.hash(password, salt);
+			// user.id = await bcrypt.hash(id, salt);
+
+			await user.save();
+
+			const payload = {
+				user: {
+					id: user.id
+				}
+			};
+
+			jwt.sign(
+				payload,
+				config.get('JWT_SECRET'),
+				{ expiresIn: 360000 },
+				(err, token) => {
+					if (err) throw err;
+					res.json({ token });
+				}
+			);
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send('Server Error!');
+		}
+
+		res.send('');
+	}
+);
 
 module.exports = router;
